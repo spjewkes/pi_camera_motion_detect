a15bfa6d4202e33adff607b5a7a5f0bb20abd468
Steve Jewkes <steve@mulus.net>
2019-05-11 10:34:53 +0100

    Added a check to make sure program does not fill up entire disk

--- motion_cam.py
+++ motion_cam.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
-from datetime import datetime
+from datetime import datetime, timedelta
 import io
 import numpy as np
+import os
 
 try:
     from picamera import PiCamera, PiCameraCircularIO
@@ -28,7 +29,8 @@ def compare(img1, img2):
 def detect_motion(camera):
     global prior_image
     global motion_enabled
-    
+    global motion_time
+
     stream = io.BytesIO()
     camera.capture(stream, format='jpeg', use_video_port=True)
     stream.seek(0)
@@ -44,12 +46,24 @@ def detect_motion(camera):
         print(result)
         # Once motion detection is done, make the prior image the current
         prior_image = current_image
-        if not motion_enabled and result > 2000:
+        if result > 1800:
+            # This resets the timer even if motion detect is already enabled. Giving a set
+            # time with no motion before stopping the camera
+            motion_time = datetime.now() + timedelta(seconds=5)
             motion_enabled = True
-        elif motion_enabled and result < 1000:
+        elif motion_enabled and result < 1000 and datetime.now() > motion_time:
             motion_enabled = False
 
     return motion_enabled
+
+def user_freespace_gb():
+    """
+    Returns the amount of free space user space in GB
+    """
+    fd = os.open('.', os.O_RDONLY | os.O_DIRECTORY)
+    info = os.fstatvfs(fd)
+    os.close(fd)
+    return (info.f_bsize * info.f_bavail) / (1024 * 1024 * 1024)
 
 def main():
     with PiCamera() as camera:
@@ -58,23 +72,30 @@ def main():
         stream = PiCameraCircularIO(camera, seconds=10)
         camera.start_recording(stream, format='h264')
         try:
+            camera.wait_recording(3)
             while True:
-                camera.wait_recording(1)
+                camera.wait_recording(0)
                 if detect_motion(camera):
                     print('Motion detected!')
                     # As soon as we detect motion, split the recording to
                     # record the frames "after" motion
                     camera.split_recording('{}_after_{:04d}.h264'.format(datetime.now().isoformat('_'), count))
                     # Write the 10 seconds "before" motion to disk as well
-                    stream.copy_to('{}_before_{:04d}.h264'.format(datetime.now().isoformat('_'), count), seconds=10)
+                    # stream.copy_to('{}_before_{:04d}.h264'.format(datetime.now().isoformat('_'), count), seconds=5)
                     stream.clear()
                     # Wait until motion is no longer detected, then split
                     # recording back to the in-memory circular buffer
                     while detect_motion(camera):
-                        camera.wait_recording(1)
+                        camera.wait_recording(0)
                     print('Motion stopped!')
                     camera.split_recording(stream)
                     count += 1
+
+                if user_freespace_gb() < 0.5:
+                    print("CLosing program to avoid using all free space.")
+                    print("There is {} GB remaining.".format(user_freespace_gb()))
+                    break
+                        
         finally:
             camera.stop_recording()
 
